fbb877287446e57572170feacf23853efefbf104
DucSon <ducsonle19@gmail.com>
2019-03-28 04:23:43 +0700

    First commit: - Login Form

---
+++ Project/LoginForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Project
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.tbUName = new System.Windows.Forms.TextBox();
+            this.tbPWord = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbUName
+            // 
+            this.tbUName.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.tbUName.Location = new System.Drawing.Point(33, 137);
+            this.tbUName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUName.Name = "tbUName";
+            this.tbUName.Size = new System.Drawing.Size(182, 24);
+            this.tbUName.TabIndex = 1;
+            this.tbUName.TabStop = false;
+            this.tbUName.Text = "Username";
+            this.tbUName.Enter += new System.EventHandler(this.tbUName_Enter);
+            this.tbUName.Leave += new System.EventHandler(this.tbUName_Leave);
+            // 
+            // tbPWord
+            // 
+            this.tbPWord.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.tbPWord.Location = new System.Drawing.Point(33, 169);
+            this.tbPWord.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPWord.Name = "tbPWord";
+            this.tbPWord.Size = new System.Drawing.Size(182, 24);
+            this.tbPWord.TabIndex = 2;
+            this.tbPWord.TabStop = false;
+            this.tbPWord.Text = "Password";
+            this.tbPWord.Enter += new System.EventHandler(this.tbPWord_Enter);
+            this.tbPWord.Leave += new System.EventHandler(this.tbPWord_Leave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(76, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(33, 212);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(182, 32);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.TabStop = false;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 294);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbPWord);
+            this.Controls.Add(this.tbUName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUName;
+        private System.Windows.Forms.TextBox tbPWord;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Project/LoginForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tbUName_Enter(object sender, EventArgs e)
+        {
+            if (tbUName.Text == "Username")
+            {
+                tbUName.Text = "";
+                tbUName.ForeColor = Color.Black;
+            }
+        }
+
+        private void tbUName_Leave(object sender, EventArgs e)
+        {
+            if (tbUName.Text == "")
+            {
+                tbUName.Text = "Username";
+                tbUName.ForeColor = Color.Gray;
+            }
+        }
+
+        private void tbPWord_Enter(object sender, EventArgs e)
+        {
+            if (tbPWord.Text == "Password")
+            {
+                tbPWord.Text = "";
+                tbPWord.ForeColor = Color.Black;
+            }
+        }
+
+        private void tbPWord_Leave(object sender, EventArgs e)
+        {
+            if (tbPWord.Text == "")
+            {
+                tbPWord.Text = "Password";
+                tbPWord.ForeColor = Color.Gray;
+            }
+        }
+    }
+}
